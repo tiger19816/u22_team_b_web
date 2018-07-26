f14db3cd72de74e19e1b889b31651da31b6371fc
Taiga Hirai <hiraitaiga16@gmail.com>
2018-07-26 11:41:37 +0900

    #1 (#7)
    * #1 APIのClassとServlet
    
    * #1 ファイル名修正
    
    * #1 IDで店の情報
    
    * commit

---
+++ src/ApiTestServlet.java
@@ -0,0 +1,43 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ApiTestServlet
+ */
+@WebServlet("/ApiTestServlet")
+public class ApiTestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApiTestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setAttribute("lat", 35.670082);
+		request.setAttribute("lon", 139.763267);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/StoreDetailsServlet.java
+++ src/StoreDetailsServlet.java
@@ -1,23 +1,27 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import b.team.works.u22.hal.u22teamb.api.ApiConnect;
+
 /**
- * Servlet implementation class TestServlet
+ * Servlet implementation class StoreDetailsServlet
  */
-@WebServlet("/TestServlet")
-public class TestServlet extends HttpServlet {
+@WebServlet("/StoreDetailsServlet")
+public class StoreDetailsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TestServlet() {
+    public StoreDetailsServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,7 +31,30 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+//		//店舗id
+		String id = request.getParameter("id");
+		// エンドポイント
+		String prmKeyid = "&keyid=" + ApiConnect.ACCKEY;
+		String prmId ="&id=" + id;
+
+		System.out.println(prmKeyid);
+		// URI組み立て
+		StringBuffer uri = new StringBuffer();
+		uri.append(ApiConnect.GNAV_URL);
+		uri.append(ApiConnect.FORMAT);
+		uri.append(prmKeyid);
+		uri.append(prmId);
+
+		// API実行、結果を取得し出力
+		String str = ApiConnect.getNodeList(uri.toString());
+		//JSONを作成する
+		response.setContentType("application/json; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		out.println(str);
+
+		System.out.println(str);
 	}
 
 	/**
