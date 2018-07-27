81636f6a828e4434f1caa61765722ce281fbdeec
takada <kikusui1kaminasizuki0@gmail.com>
2018-07-27 09:15:34 +0900

    Kwhr : エラー無しを確認済みです。 (#8)
    * テスト用サーブレット、SQL文格納クラス、妻のエンティティクラス作成
    
    * 夫エンティティクラス、店エンティティクラス作成
    妻エンティティクラス側の夫IDを夫エンティティクラスと接続（？）
    
    * 予約エンティティクラス、メニューエンティティクラスの作成
    【疑問点】予約テーブルの「項番」はメニューテーブルの外部キーでなくてもいいのか。
    
    * 妻IDから妻情報、夫情報を抽出しJSONで出力する機能完了
    
    * エンティティクラス微修正
    
    * DB内容更新により微修正＋必要ないかもだけどVIEW作成したのでディレクトリに追加しときます。
    
    * DBの変更に伴うソースの変更。
    
    * データ内容の変更
    
    * アイコンのゲッターセッター追加
    
    * jspファイル変更
    
    * テスト用
    
    * コミット
    
    * jspファイルを訂正
    
    * 変更をコミット
    
    * 訂正コミット
    
    * 予約リスト用のソースを作成
    
    * 履歴リスト表示用のソース記述
    
    * DataAccessに追加
    
    * 妻側の予約テーブルの表示完了
    
    * 履歴リスト用の処理を追加
    
    * 競合の編集
    
    * 一応コミット

--- src/HistoryListJsonServlet.java
+++ src/HistoryListJsonServlet.java
@@ -14,16 +14,16 @@ import javax.servlet.http.HttpServletResponse;
 import b.team.works.u22.hal.u22teamb.tools.DataAccess;
 
 /**
- * Servlet implementation class ReservationListJsonServlet
+ * Servlet implementation class HistoryListJsonServlet
  */
-@WebServlet("/ReservationListJsonServlet")
-public class ReservationListJsonServlet extends HttpServlet {
+@WebServlet("/HistoryListJsonServlet")
+public class HistoryListJsonServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReservationListJsonServlet() {
+    public HistoryListJsonServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,32 +33,32 @@ public class ReservationListJsonServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//文字化け対策
-		request.setCharacterEncoding("utf-8");
+				//文字化け対策
+				request.setCharacterEncoding("utf-8");
 
-		//妻ID取得
-		String femaleId = request.getParameter("id");
+				//妻ID取得
+				String femaleId = request.getParameter("id");
 
-		//行毎のデータ
-		ArrayList<ArrayList<String>> reservationList = new ArrayList<ArrayList<String>>();
+				//行毎のデータ
+				ArrayList<ArrayList<String>> historyList = new ArrayList<ArrayList<String>>();
 
-		//DBに接続
-		DataAccess da = null;
-		try {
-			da = new DataAccess();
-			reservationList = da.ReservationListSelect(Integer.parseInt(femaleId));
-			da.close();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+				//DBに接続
+				DataAccess da = null;
+				try {
+					da = new DataAccess();
+					historyList = da.HistoryListSelect(Integer.parseInt(femaleId));
+					da.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
 
-		request.setAttribute("LIST", reservationList);
-		RequestDispatcher rd = request.getRequestDispatcher("reservation_list_json.jsp");
-		rd.forward(request, response);
+				request.setAttribute("LIST", historyList);
+				RequestDispatcher rd = request.getRequestDispatcher("history_list_json.jsp");
+				rd.forward(request, response);
 	}
 
 	/**
