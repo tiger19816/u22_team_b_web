9443f4f5ddded6a019b7d38ee0915f9642cb09ca
takada <kikusui1kaminasizuki0@gmail.com>
2018-08-22 14:12:42 +0900

    Kwhr ユーザー新規登録の調整 (#20)
    * テスト用サーブレット、SQL文格納クラス、妻のエンティティクラス作成
    
    * 夫エンティティクラス、店エンティティクラス作成
    妻エンティティクラス側の夫IDを夫エンティティクラスと接続（？）
    
    * 予約エンティティクラス、メニューエンティティクラスの作成
    【疑問点】予約テーブルの「項番」はメニューテーブルの外部キーでなくてもいいのか。
    
    * 妻IDから妻情報、夫情報を抽出しJSONで出力する機能完了
    
    * エンティティクラス微修正
    
    * DB内容更新により微修正＋必要ないかもだけどVIEW作成したのでディレクトリに追加しときます。
    
    * DBの変更に伴うソースの変更。
    
    * データ内容の変更
    
    * アイコンのゲッターセッター追加
    
    * jspファイル変更
    
    * テスト用
    
    * コミット
    
    * jspファイルを訂正
    
    * 変更をコミット
    
    * 訂正コミット
    
    * 予約リスト用のソースを作成
    
    * 履歴リスト表示用のソース記述
    
    * DataAccessに追加
    
    * 妻側の予約テーブルの表示完了
    
    * 履歴リスト用の処理を追加
    
    * 競合の編集
    
    * 一応コミット
    
    * ユーザーログインの処理を記述
    
    * ログイン処理の修正
    
    * 予約リストの1データ分取得するソースを記述
    
    * 訂正
    
    * 予約内容UPDATE用の処理記述
    
    * 予約内容UPDATEのソース変更
    
    * 修正
    
    * 予約リスト、履歴リストの変更
    
    * 店のログイン等の処理
    
    * 夫の新規登録処理完了
    
    * マージ後の訂正
    
    * ユーザーの新規登録変更
    
    * ユーザー新規登録調整

--- src/b/team/works/u22/hal/u22teamb/tools/AccessDao.java
+++ src/b/team/works/u22/hal/u22teamb/tools/AccessDao.java
@@ -2,6 +2,8 @@ package b.team.works.u22.hal.u22teamb.tools;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class AccessDao extends Dao{
 
 	public AccessDao() throws SQLException, ClassNotFoundException {
@@ -62,6 +64,80 @@ public class AccessDao extends Dao{
 			// TODO: handle exception
 		}
 	}
+
+
+    public int userInsert(String[] maleDatas , String[] femaleDatas) {
+    		int result = 0;
+		try {
+			//夫情報を登録。
+			String male_sql = "INSERT INTO male(name,mail,password,birthday,height,weight,profession)";
+			male_sql += " VALUES(?,?,?,?,?,?,?)";
+			this.pst = cn.prepareStatement(male_sql);
+			pst.setString(1, maleDatas[0]);
+			pst.setString(2, maleDatas[1]);
+			pst.setString(3, maleDatas[2]);
+			pst.setString(4, maleDatas[3]);
+			pst.setString(5, maleDatas[4]);
+			pst.setString(6, maleDatas[5]);
+			pst.setString(7, maleDatas[6]);
+			int flag = pst.executeUpdate();
+			System.out.println(flag);
+			//夫ID取得。
+			if(flag>0) {
+				pst = cn.prepareStatement("SELECT last_insert_id()");
+				this.rs = this.pst.executeQuery();
+				String maleId = "";
+				if(rs.next()) {
+					result = rs.getInt("last_insert_id()");
+					maleId = String.valueOf(result);
+				}
+
+				//妻情報を登録。
+				String female_sql = "INSERT INTO female(name,birthday,mail,password,icon_url,card_number,card_expiration_date,card_security_code,card_nominee,address,point_latitude,point_longitude,male_id)";
+				female_sql += " VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+				this.pst = cn.prepareStatement(female_sql);
+				pst.setString(1, femaleDatas[0]);
+				pst.setString(2, femaleDatas[1]);
+				pst.setString(3, femaleDatas[2]);
+				pst.setString(4, femaleDatas[3]);
+				pst.setString(5, femaleDatas[4]);
+				pst.setString(6, femaleDatas[5]);
+				pst.setString(7, femaleDatas[6]);
+				pst.setString(8, femaleDatas[7]);
+				pst.setString(9, femaleDatas[8]);
+				pst.setString(10, femaleDatas[9]);
+				pst.setString(11, femaleDatas[10]);
+				pst.setString(12, femaleDatas[11]);
+				pst.setString(13, String.valueOf(result));
+				flag = pst.executeUpdate();
+				System.out.println(flag);
+				//妻ID取得。
+				result = 0;
+				if(flag>0) {
+					pst = cn.prepareStatement("SELECT last_insert_id()");
+					this.rs = this.pst.executeQuery();
+					if(rs.next()) {
+						result = rs.getInt("last_insert_id()");
+					}
+				}
+
+				String text = RandomStringUtils.randomAlphabetic(15);
+				String text2 = RandomStringUtils.random(10, text);
+				String sql = "INSERT INTO team_b_db.signupcode(id,female_id,male_id) VALUES (?,?,?);";
+				this.pst = cn.prepareStatement(female_sql);
+				pst.setString(1, text2);
+				pst.setString(2, String.valueOf(result));
+				pst.setString(3, maleId);
+				flag = pst.executeUpdate();
+			}
+
+			return result;
+
+		} catch (SQLException e) {
+			// TODO: handle exception
+			return 0;
+		}
+    }
 
     /**
      * 予約内容のUPDATE用メソッド。
