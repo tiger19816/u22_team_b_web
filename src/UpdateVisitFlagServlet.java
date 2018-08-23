f7a7f5d27bc4fb6037c4b350d4667fdde8eefd92
Yuki Yoshida <osaka.hal.yoshida@gmail.com>
2018-08-24 08:51:36 +0900

    ・masterをマージ。

--- src/UpdateVisitFlagServlet.java
+++ src/UpdateVisitFlagServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import b.team.works.u22.hal.u22teamb.tools.DataAccess;
+import b.team.works.u22.hal.u22teamb.tools.MyMessage;
 
 /**
  * 来店処理を行うサーブレット。
@@ -35,10 +36,20 @@ public class UpdateVisitFlagServlet extends HttpServlet {
 		
 		String reservationId = request.getParameter("reservation_id");
 		
+		String title = "U22夫管理アプリ";
+		String message = "さんが店舗へ来店しました！";
+		
 		try {
 			DataAccess da = new DataAccess();
 			da.updateVisitFlag(reservationId);
+			
+			String token = "";
+			String male = "";
+			
 			// この後、妻に通知したい。
+//			MyMessage push = new MyMessage(title, male + message);
+//			push.sendToToken(token);
+			
 			da.close();
 		} catch (Exception e) {
 			e.printStackTrace();
